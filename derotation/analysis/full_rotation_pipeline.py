02e4a9504b922aa00700f26e4f2cc042c4cceedd
lauraporta <29216006+lauraporta@users.noreply.github.com>
2024-07-04 15:19:23 +0100

    Remove contrast enhancement

--- derotation/analysis/full_rotation_pipeline.py
+++ derotation/analysis/full_rotation_pipeline.py
@@ -55,7 +55,7 @@ class FullPipeline:
         - adding a circular mask to the rotated image stack
         - saving the masked image stack
         """
-        self.contrast_enhancement()
+        # self.contrast_enhancement()
         self.process_analog_signals()
         rotated_images = self.rotate_frames_line_by_line()
         masked = self.add_circle_mask(rotated_images, self.mask_diameter)
@@ -172,44 +172,44 @@ class FullPipeline:
         logging.info(f"Dataset {self.filename_raw} loaded")
         logging.info(f"Filename: {self.filename}")
 
-    def contrast_enhancement(self):
-        """Applies contrast enhancement to the image stack.
-        It is useful to visualize the image stack before derotation.
-        """
-        logging.info("Applying contrast enhancement...")
+    # def contrast_enhancement(self):
+    #     """Applies contrast enhancement to the image stack.
+    #     It is useful to visualize the image stack before derotation.
+    #     """
+    #     logging.info("Applying contrast enhancement...")
 
-        self.image_stack = np.array(
-            [
-                self.contrast_enhancement_single_image(
-                    img, self.config["contrast_enhancement"]
-                )
-                for img in self.image_stack
-            ]
-        )
+    #     self.image_stack = np.array(
+    #         [
+    #             self.contrast_enhancement_single_image(
+    #                 img, self.config["contrast_enhancement"]
+    #             )
+    #             for img in self.image_stack
+    #         ]
+    #     )
 
-    @staticmethod
-    def contrast_enhancement_single_image(
-        img: np.ndarray, saturated_percentage=0.35
-    ) -> np.ndarray:
-        """Applies contrast enhancement to a single image.
-        It is useful to visualize the image stack before derotation.
+    # @staticmethod
+    # def contrast_enhancement_single_image(
+    #     img: np.ndarray, saturated_percentage=0.35
+    # ) -> np.ndarray:
+    #     """Applies contrast enhancement to a single image.
+    #     It is useful to visualize the image stack before derotation.
 
-        Parameters
-        ----------
-        img : np.ndarray
-            The image to enhance.
-        saturated_percentage : float, optional
-            The percentage of saturated pixels, by default 0.35
+    #     Parameters
+    #     ----------
+    #     img : np.ndarray
+    #         The image to enhance.
+    #     saturated_percentage : float, optional
+    #         The percentage of saturated pixels, by default 0.35
 
-        Returns
-        -------
-        np.ndarray
-            The enhanced image.
-        """
-        v_min, v_max = np.percentile(
-            img, (saturated_percentage, 100 - saturated_percentage)
-        )
-        return rescale_intensity(img, in_range=(v_min, v_max))
+    #     Returns
+    #     -------
+    #     np.ndarray
+    #         The enhanced image.
+    #     """
+    #     v_min, v_max = np.percentile(
+    #         img, (saturated_percentage, 100 - saturated_percentage)
+    #     )
+    #     return rescale_intensity(img, in_range=(v_min, v_max))
 
     def process_analog_signals(self):
         """From the analog signals (frame clock, line clock, full rotation,
@@ -470,8 +470,8 @@ class FullPipeline:
             raise ValueError(
                 "Start and end of rotations have different lengths"
             )
-        # if self.rot_blocks_idx["start"].shape[0] != self.number_of_rotations:
-        #     raise ValueError("Number of rotations is not as expected")
+        if self.rot_blocks_idx["start"].shape[0] != self.number_of_rotations:
+            raise ValueError("Number of rotations is not as expected")
 
         logging.info("Number of rotations is as expected")
 
@@ -612,7 +612,7 @@ class FullPipeline:
         rotation_end = np.where(np.diff(thresholded) < 0)[0]
 
         assert len(rotation_start) == len(rotation_end)
-        # assert len(rotation_start) == self.number_of_rotations
+        assert len(rotation_start) == self.number_of_rotations
 
         for i, (start, end) in enumerate(
             zip(rotation_start[1:], rotation_end[:-1])

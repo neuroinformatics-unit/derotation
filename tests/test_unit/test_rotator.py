c913a06a552e358e637916a013c4e01c8d733f5b
lauraporta <29216006+lauraporta@users.noreply.github.com>
2024-09-25 17:40:25 +0100

    Add basic tests

---
+++ tests/test_unit/test_rotator.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from derotation.simulate.basic_rotator import Rotator
+
+
+def test_Rotator_constructor():
+    # image stack with 3 frames of 100x100 pixels
+    image_stack = np.zeros((3, 100, 100))
+    # 300 angles, one per line per frame
+    angles = np.arange(300)
+    # create a Rotator object
+    rotator = Rotator(angles, image_stack)
+    # check if the object was created correctly
+    assert rotator.angles == angles
+    assert rotator.image_stack == image_stack
+
+
+def test_failing_Rotator_instantiation():
+    # image stack with 3 frames of 100x100 pixels
+    image_stack = np.zeros((3, 100, 100))
+    # 299 angles, one per line per frame
+    angles = np.arange(299)
+    # create a Rotator object
+    try:
+        Rotator(angles, image_stack)
+    except AssertionError:
+        assert True
+    else:
+        assert False

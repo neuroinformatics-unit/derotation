ca6cc0368e14b92ce0633b63d19963fda35987fb
lauraporta <29216006+lauraporta@users.noreply.github.com>
2024-09-30 15:28:47 +0100

    Plotting bug fixed

--- derotation/derotate_by_line.py
+++ derotation/derotate_by_line.py
@@ -166,7 +166,7 @@ def derotate_an_image_array_line_by_line(
 
             if plotting_hook_image_completed is not None:
                 plotting_hook_image_completed(
-                    derotated_filled_image, image_counter
+                    derotated_image_stack, image_counter
                 )
 
     return derotated_image_stack

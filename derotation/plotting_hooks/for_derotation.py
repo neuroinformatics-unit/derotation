0dd3c6d74420e65d07cd0e3e7dacf8a3f7a7fe90
lauraporta <29216006+lauraporta@users.noreply.github.com>
2024-09-30 15:28:06 +0100

    Change variable names in plotting hooks

--- derotation/plotting_hooks/for_derotation.py
+++ derotation/plotting_hooks/for_derotation.py
@@ -1,13 +1,15 @@
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def line_addition(
-    rotated_filled_image, rotated_line, image_counter, line_counter, angle
+    derotated_filled_image, rotated_line, image_counter, line_counter, angle
 ):
     fig, ax = plt.subplots(1, 2, figsize=(10, 10))
 
-    ax[0].imshow(rotated_filled_image, cmap="viridis")
+    ax[0].imshow(derotated_filled_image, cmap="viridis")
     ax[0].set_title(f"Frame {image_counter}")
     ax[0].axis("off")
 
@@ -15,6 +17,7 @@ def line_addition(
     ax[1].set_title(f"Line {line_counter}, angle: {angle:.2f}")
     ax[1].axis("off")
 
+    Path("debug/lines/").mkdir(parents=True, exist_ok=True)
     plt.savefig(
         f"debug/lines/derotated_image_{image_counter}_line_{line_counter}.png",
         dpi=300,
@@ -22,7 +25,7 @@ def line_addition(
     plt.close()
 
 
-def image_completed(rotated_image_stack, image_counter):
+def image_completed(derotated_image_stack, image_counter):
     """Hook for plotting the image stack after derotation.
     It is useful for debugging purposes.
     """
@@ -30,17 +33,17 @@ def image_completed(rotated_image_stack, image_counter):
         fig, ax = plt.subplots(1, 1, figsize=(10, 10))
         #  plot maximum projection of the image stack
         ax.imshow(
-            np.max(rotated_image_stack[:image_counter], axis=0),
+            np.max(derotated_image_stack[:image_counter], axis=0),
             cmap="viridis",
         )
         ax.axis("off")
+        Path("debug/").mkdir(parents=True, exist_ok=True)
         plt.savefig("debug/max_projection.png", dpi=300)
         plt.close()
 
-    plt.close()
-
-    fig, ax = plt.subplots(1, 1, figsize=(10, 10))
-    ax.imshow(rotated_image_stack[image_counter], cmap="viridis")
-    ax.axis("off")
-    plt.savefig(f"debug/frames/derotated_image_{image_counter}.png", dpi=300)
-    plt.close()
+    # fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+    # ax.imshow(derotated_image_stack[image_counter], cmap="viridis")
+    # ax.axis("off")
+    # Path(f"debug/frames/").mkdir(parents=True, exist_ok=True)
+    # plt.savefig(f"debug/frames/derotated_image_{image_counter}.png", dpi=300)
+    # plt.close()

dddbaf3e50042f6d1d85cfebf7b5a79979c9f2ff
lauraporta <29216006+lauraporta@users.noreply.github.com>
2024-10-03 14:12:30 +0100

    Reorganize examples

--- examples/derotate.py
+++ examples/derotate.py
@@ -1,18 +1,4 @@
 from derotation.analysis.full_derotation_pipeline import FullPipeline
-from derotation.analysis.incremental_derotation_pipeline import (
-    IncrementalPipeline,
-)
-from derotation.plotting_hooks.for_derotation import (
-    image_completed,
-)
 
-derotator_incremental = IncrementalPipeline("incremental_rotation")
-derotator_incremental()
-hooks = {
-    # "plotting_hook_line_addition": line_addition,
-    "plotting_hook_image_completed": image_completed,
-}
 derotate = FullPipeline("full_rotation")
-derotate.hooks = hooks
-derotate.center_of_rotation = derotator_incremental.center_of_rotation
 derotate()

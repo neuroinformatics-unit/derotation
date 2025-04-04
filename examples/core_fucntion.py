a0d28f85bd693021b823c5222c2ef96c6af440f8
lauraporta <29216006+lauraporta@users.noreply.github.com>
2025-04-04 14:50:42 +0100

    Improve example on core function

--- examples/core_fucntion.py
+++ examples/core_fucntion.py
@@ -27,11 +27,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tifffile
 
-# Local module for derotation
 from derotation.derotate_by_line import derotate_an_image_array_line_by_line
 
 # %%
-# Set up paths
+# Set up
+# -----------
 
 current_module_path = Path.cwd()
 data_folder = current_module_path / "data"
@@ -54,6 +54,19 @@ print(f"Image stack shape: {image_stack.shape}")
 print(f"Angles per line shape: {angles_per_line.shape}")
 
 # %%
+# See the angles
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.scatter(np.arange(angles_per_line.shape[0]), angles_per_line, s=1)
+ax.set_title("Rotation angles per line")
+ax.set_xlabel("Line number")
+ax.set_ylabel("Angle (degrees)")
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+plt.tight_layout()
+plt.show()
+
+
+# %%
 # Set derotation parameters
 # see :meth:`derotation.analysis.full_derotation_pipeline.FullPipeline
 # .find_image_offset` for more details on finding the offset value
@@ -61,6 +74,16 @@ rotation_center = (129, 128)  # (y, x) format
 blank_pixels_value = -3623  # Value for pixels outside the rotated area
 
 print(f"Applying derotation with center: {rotation_center}")
+
+# %%
+#  plot first frame with rotation center
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.imshow(image_stack[0], cmap="gray")
+ax.scatter(rotation_center[1], rotation_center[0], color="red", s=20)
+ax.set_title("Rotation center")
+ax.axis("off")
+plt.tight_layout()
+plt.show()
 
 # %%
 # Perform line-by-line derotation
